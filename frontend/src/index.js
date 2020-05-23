3a2dc29aefa71126fc35d9d59d793a0c7e9bf213
Arpit Bhalla <arpitbhalla2001@gmail.com>
2020-05-23 20:41:32 +0530

    feat: Admin Panel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import Match from "./screens/Match/Match";
+import Admin from "./screens/Admin/Admin";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,8 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
       <Router>
           <Switch>
-              <Route exact path="/">{"Home"}</Route>
+              <Route exact path="/">{"Dashboard"}</Route>
               <Route exact path="/admin">{"ADmin Panel"}</Route>
+              {/* <Route exact path="/auth">{"Auth"}</Route> */}
               <Route exact path="/m/:matchID"><Match/></Route>
               <Route exact path="/m/:matchID/console">{"Match Console Panel"}</Route>
               <Route>{"404 Not Found"}</Route>
---
+++ frontend/src/screens/Admin/Admin.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Admin = () => {
+    return (
+        <Fragment>
+            dsg
+        </Fragment>
+    );
+}
+
+export default Admin;
