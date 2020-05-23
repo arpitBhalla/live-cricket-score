1a3427916ddec77943aeaa4d83b4660c283e2504
Arpit Bhalla <arpitbhalla2001@gmail.com>
2020-05-23 09:56:47 +0530

    Server Complete

--- frontend/server/routes/route.js
+++ frontend/server/routes/route.js
@@ -1,10 +1,44 @@
 var express = require('express')
 var router = express.Router()
 
-router.use((req, res, next)=>next())
+let model = require("./../Models/Match")
+// let model
+const response = require('./sendResponse')
 
-router.use("/m",require("./match"))
-// router.use("/p",require("./match"))
-// router.use("/t",require("./match"))
+// router.use((req, res, next) => next())
+
+router.param("mode", (req, res, next, mode) => {
+  switch (mode) {
+    case 'm':
+      model = require("./../Models/Match")
+      break;
+    case 't':
+      model = require("./../Models/Match")
+      break;
+    case 'p':
+      model = require("./../Models/Player")
+      break;
+    default:
+      return response(res, "SomeThing Went Wrong", "")
+      break;
+  }
+  next()
+})
+
+router
+  .post("/:mode(m|t|p)", (req, res) => {
+    model.create(req.body, (err, data) => response(res, err, data))
+  })
+
+router.route("/:mode(m|t|p)/:id")
+  .get((req, res) => {
+    model.findById(req.params.id, (err, data) => response(res, err, data))
+  })
+  .put((req, res) => {
+    model.findByIdAndUpdate(req.param.id, req.body, (err, data) => response(res, err, data))
+  })
+  .delete((req, res) => {
+    model.findByIdAndDelete(req.params.id, (err, data) => response(res, err, data))
+  })
 
 module.exports = router
